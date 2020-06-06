64ea4de273591fd267afc4510e227a837ec9aea8
jason jafari <jason.jafari.m@gmail.com>
2020-06-05 21:29:42 -0400

    litreral and factory

---
+++ 002-Factories/index.js
@@ -0,0 +1,15 @@
+// Factory Function
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+circle.draw();
+
